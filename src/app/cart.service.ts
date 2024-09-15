f4d0b2c4faae5ec7988a071e4261e4a350e1143d
Abdelrahman <abdelrahman.abdalla@stemredsea.moe.edu.eg>
2024-09-15 20:30:22 +0300

    update and remove cart item

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 interface Product {
   id: number;
@@ -7,45 +9,100 @@ interface Product {
   pictureUrl: string;
   categoryName: string;
 }
-
+interface AddToCart {
+  productId: number;
+  quantity: number;
+}
+interface CartItem {
+  id: number;
+  productId: number;
+  productName: string;
+  quantity: number;
+  price: number;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  cartItems: { product: Product, quantity: number }[] = [];
+   cartItems: CartItem[] = [];
+   addItem: AddToCart[] = [];
 
-  constructor() { }
+   private apiUrl = 'https://localhost:7193/CartItem/Add';
+
+   constructor(private http: HttpClient) { }
+
+
+   loadCartItems(): void {
+    this.http.get<CartItem[]>('https://localhost:7193/CartItem/GetLookUp').subscribe(
+      items => {
+        this.cartItems = items;
+      },
+      error => {
+        console.error('Error fetching cart items:', error);
+  
+      }
+    );
+  }
+
+  addToCartBE(addItem: AddToCart): Observable<AddToCart> {
+    return this.http.post<AddToCart>(this.apiUrl, addItem);
+  }
 
   addToCart(product: Product): void {
-    const existingItem = this.cartItems.find(item => item.product === product);
+    const existingItem = this.cartItems.find(item => item.productId=== product.id);
     if (existingItem) {
       existingItem.quantity++;
     } else {
-      this.cartItems.push({ product, quantity: 1 });
+      const addToCart: AddToCart = {
+        productId: product.id,
+        quantity: 1
+      };
+      this.addToCartBE(addToCart).subscribe(response => {
+        this.addItem.push(response);
+      }, error => {
+        console.error('Error fetching products:', error);
+      });
     }
   }
-  
 
-  removeFromCart(product: Product): void {
-    this.cartItems = this.cartItems.filter(item => item.product !== product);
+  removeFromCartService(id: number): Observable<void> {
+    return this.http.delete<void>(`${"https://localhost:7193/CartItem/Delete"}/${id}`);
   }
-
-  updateQuantity(product: Product, quantity: number): void {
-    const item = this.cartItems.find(item => item.product === product);
+  removeFromCart(cartId: number): void {
+    const cartItems = this.cartItems[cartId];
+    this.removeFromCartService(cartId).subscribe(() => {
+      this.cartItems.splice(cartId, 1);
+    }, error => {
+      console.error('Error fetching products:', error);
+    });
+  }
+  updateCartItem(id: number, quantity: Number): Observable<void> {
+    return this.http.put<void>(`${"https://localhost:7193/CartItem/UpdateQuantity"}/${id}`, { quantity });
+  }
+  
+  updateQuantity(cartId: number, productId: number, quantity: number): void {
+    const item = this.cartItems.find(item => item.id === cartId);
     if (item) {
       item.quantity = quantity;
       if (quantity === 0) {
-        this.removeFromCart(product);
+        this.removeFromCart(cartId);
       }
+      this.updateCartItem(cartId, quantity).subscribe(() => {
+      }, error => {
+        console.error('Error fetching products:', error);
+      });
     }
+
   }
 
   isProductInCart(product: Product): boolean {
-    return this.cartItems.some(item => item.product === product);
+    return this.cartItems.some(item => item.productId === product.id);
   }
 
-  getProductQuantity(product: Product): number {
-    const item = this.cartItems.find(item => item.product === product);
+  getProductQuantity(cartItemId: number): number {
+    const item = this.cartItems.find(item => item.id === cartItemId);
     return item ? item.quantity : 0;
   }
+
+  
 }
